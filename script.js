84c48cf6bc7856df0f3ea945c0eae074f858f2c9
MinhKhue289 <eoviti06@gmail.com>
2025-03-23 17:53:16 +0700

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,21 @@ document.getElementById("client").addEventListener("click",function() {
         behavior: "smooth"
     })
 });
+function checkAccess() {
+  let password = prompt("T có đẹp trai không? Yes or No !!!")
+    .trim()
+    .toLowerCase();
+
+  if (password === "yes") {
+    alert("Ok");
+    window.location.href = "https://minhkhue289.github.io/Web-5/";
+  } else if (password === null || password === "") {
+    alert("Bạn chưa nhập gì cả!");
+  } else {
+    alert("Sai rồi !!! Hãy thử lại.");
+    checkAccess();
+  }
+}
+
+// Gọi hàm khi trang load
+checkAccess();
