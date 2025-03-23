3787502f8736bba1709edd96e057ed271249a788
MinhKhue289 <eoviti06@gmail.com>
2025-03-23 18:01:17 +0700

    Update script.js

--- script.js
+++ script.js
@@ -9,26 +9,4 @@ document.getElementById("client").addEventListener("click",function() {
         behavior: "smooth"
     })
 });
-function checkAccess() {
-    // Nếu đã nhập đúng trước đó, bỏ qua check
-    if (localStorage.getItem("accessGranted") === "true") {
-        return;
-    }
-
-    let password = prompt("T có đẹp trai không? Yes or No !!!").trim().toLowerCase();
-    
-    if (password === "yes") {
-        alert("Ok");
-        localStorage.setItem("accessGranted", "true"); // Lưu vào localStorage
-        window.location.href = "https://minhkhue289.github.io/Web-5/";
-    } else if (password === null || password === "") {
-        alert("Bạn chưa nhập gì cả!");
-    } else {
-        alert("Sai rồi !!! Hãy thử lại.");
-        checkAccess();
-    }
-}
-
-// Gọi hàm khi trang load
-checkAccess();
 
